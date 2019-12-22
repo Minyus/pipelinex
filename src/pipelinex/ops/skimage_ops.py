99796252bc870348d87a6d90dd27942751779704
Minyus <yusuke.minami86@gmail.com>
2019-12-22 10:19:19 +0800

    Add more segmentation algorithms

--- src/pipelinex/ops/skimage_ops.py
+++ src/pipelinex/ops/skimage_ops.py
@@ -6,8 +6,20 @@ class SkimageSegmentationDictToDict(DictToDict):
     module = segmentation
 
 
+class SkimageSegmentationFelzenszwalb(SkimageSegmentationDictToDict):
+    fn = "felzenszwalb"
+
+
 class SkimageSegmentationSlic(SkimageSegmentationDictToDict):
     fn = "slic"
+
+
+class SkimageSegmentationQuickshift(SkimageSegmentationDictToDict):
+    fn = "quickshift"
+
+
+class SkimageSegmentationWatershed(SkimageSegmentationDictToDict):
+    fn = "watershed"
 
 
 class SkimageMarkBoundaries(SkimageSegmentationDictToDict):
