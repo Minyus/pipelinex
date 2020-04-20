3987ff3eba2810a7afbc1dcd4dc035d697e7126a
Minyus <yusuke.minami86@gmail.com>
2020-04-20 00:38:27 +0000

    Add test for mem_profile

---
+++ src/tests/decorators/test_memory_profiler.py
@@ -0,0 +1,15 @@
+
+from pipelinex.decorators.memory_profiler import mem_profile
+from time import sleep
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+@mem_profile
+def sleeping_identity(inp):
+    sleep(0.1)
+    return inp
+
+
+sleeping_identity(1)
