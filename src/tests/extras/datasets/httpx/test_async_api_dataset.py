18e969a7112001b5ccb631248b8df8673db4711d
Minyus <yusuke.minami86@gmail.com>
2020-05-13 23:32:38 +0800

    Add testing calling twice; Remove trivial tests

--- src/tests/extras/datasets/httpx/test_async_api_dataset.py
+++ src/tests/extras/datasets/httpx/test_async_api_dataset.py
@@ -29,132 +29,11 @@
 # pylint: disable=no-member
 import PIL
 import io
-import socket
 
 import pytest
-import requests
-import requests_mock
 
 from pipelinex import AsyncAPIDataSet as APIDataSet
 from kedro.io.core import DataSetError
-
-POSSIBLE_METHODS = ["GET", "OPTIONS", "HEAD", "POST", "PUT", "PATCH", "DELETE"]
-
-TEST_URL = "http://example.com/api/test"
-TEST_TEXT_RESPONSE_DATA = "This is a response."
-TEST_JSON_RESPONSE_DATA = [{"key": "value"}]
-
-TEST_PARAMS = {"param": "value"}
-TEST_URL_WITH_PARAMS = TEST_URL + "?param=value"
-
-TEST_HEADERS = {"key": "value"}
-
-
-@pytest.mark.parametrize("method", POSSIBLE_METHODS)
-class TestAPIDataSet:
-    @pytest.fixture
-    def requests_mocker(self):
-        with requests_mock.Mocker() as mock:
-            yield mock
-
-    def test_successfully_load_with_text_response(self, requests_mocker, method):
-        api_data_set = APIDataSet(
-            url=TEST_URL, method=method, params=TEST_PARAMS, headers=TEST_HEADERS
-        )
-        requests_mocker.register_uri(
-            method,
-            TEST_URL_WITH_PARAMS,
-            headers=TEST_HEADERS,
-            text=TEST_TEXT_RESPONSE_DATA,
-        )
-
-        response = api_data_set.load()
-        assert response == TEST_TEXT_RESPONSE_DATA
-
-    def test_successfully_load_with_json_response(self, requests_mocker, method):
-        api_data_set = APIDataSet(
-            url=TEST_URL,
-            method=method,
-            params=TEST_PARAMS,
-            headers=TEST_HEADERS,
-            attribute="json",
-        )
-        requests_mocker.register_uri(
-            method,
-            TEST_URL_WITH_PARAMS,
-            headers=TEST_HEADERS,
-            json=TEST_JSON_RESPONSE_DATA,
-        )
-
-        response = api_data_set.load()
-        assert response == TEST_JSON_RESPONSE_DATA
-
-    def test_http_error(self, requests_mocker, method):
-        api_data_set = APIDataSet(
-            url=TEST_URL, method=method, params=TEST_PARAMS, headers=TEST_HEADERS
-        )
-        requests_mocker.register_uri(
-            method,
-            TEST_URL_WITH_PARAMS,
-            headers=TEST_HEADERS,
-            text="Nope, not found",
-            status_code=requests.codes.FORBIDDEN,
-        )
-
-        with pytest.raises(DataSetError, match="Failed to fetch data"):
-            api_data_set.load()
-
-    def test_socket_error(self, requests_mocker, method):
-        api_data_set = APIDataSet(
-            url=TEST_URL, method=method, params=TEST_PARAMS, headers=TEST_HEADERS
-        )
-        requests_mocker.register_uri(method, TEST_URL_WITH_PARAMS, exc=socket.error)
-
-        with pytest.raises(DataSetError, match="Failed to connect"):
-            api_data_set.load()
-
-    def test_read_only_mode(self, method):
-        """
-        Saving is disabled on the data set.
-        """
-        api_data_set = APIDataSet(url=TEST_URL, method=method)
-        with pytest.raises(DataSetError, match="is a read only data set type"):
-            api_data_set.save({})
-
-    def test_exists_http_error(self, requests_mocker, method):
-        """
-        In case of an unexpected HTTP error,
-        ``exists()`` should not silently catch it.
-        """
-        api_data_set = APIDataSet(
-            url=TEST_URL, method=method, params=TEST_PARAMS, headers=TEST_HEADERS
-        )
-        requests_mocker.register_uri(
-            method,
-            TEST_URL_WITH_PARAMS,
-            headers=TEST_HEADERS,
-            text="Nope, not found",
-            status_code=requests.codes.FORBIDDEN,
-        )
-        with pytest.raises(DataSetError, match="Failed to fetch data"):
-            api_data_set.exists()
-
-    def test_exists_ok(self, requests_mocker, method):
-        """
-        If the file actually exists and server responds 200,
-        ``exists()`` should return True
-        """
-        api_data_set = APIDataSet(
-            url=TEST_URL, method=method, params=TEST_PARAMS, headers=TEST_HEADERS
-        )
-        requests_mocker.register_uri(
-            method,
-            TEST_URL_WITH_PARAMS,
-            headers=TEST_HEADERS,
-            text=TEST_TEXT_RESPONSE_DATA,
-        )
-
-        assert api_data_set.exists()
 
 
 def test_successfully_load_with_content_response():
@@ -249,6 +128,32 @@ def test_successfully_load_from_url_list_with_content_response_by_call():
             }
         },
     )
+    content_list = api_data_set(
+        url=[foo_image_url, bar_image_url], method="GET", attribute="content",
+    )
+    assert isinstance(content_list, list)
+    for content in content_list:
+        assert content[1:4] == b"PNG"  # part of PNG file signature
+
+
+def test_successfully_load_from_url_list_with_content_response_by_call_twice():
+    api_data_set = APIDataSet(
+        pool_config={
+            foobar_prefix: {
+                "pool_connections": 1,
+                "pool_maxsize": 1,
+                "max_retries": 0,
+                "pool_block": False,
+            }
+        },
+    )
+    content_list = api_data_set(
+        url=[foo_image_url, bar_image_url], method="GET", attribute="content",
+    )
+    assert isinstance(content_list, list)
+    for content in content_list:
+        assert content[1:4] == b"PNG"  # part of PNG file signature
+
     content_list = api_data_set(
         url=[foo_image_url, bar_image_url], method="GET", attribute="content",
     )
