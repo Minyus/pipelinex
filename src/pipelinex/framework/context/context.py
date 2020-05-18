c686963bbdc7172c1749f2f4a2763ac82910e578
Minyus <yusuke.minami86@gmail.com>
2020-05-18 11:09:38 +0800

    Support kedro <= 0.15.9

--- src/pipelinex/framework/context/context.py
+++ src/pipelinex/framework/context/context.py
@@ -2,6 +2,6 @@ import kedro
 
 try:
     from kedro.framework.context import *  # NOQA
-except ImportError:
+except (ImportError, ModuleNotFoundError):
     # if kedro.__version__ <= "0.15.9"
     from kedro.context import *  # NOQA
