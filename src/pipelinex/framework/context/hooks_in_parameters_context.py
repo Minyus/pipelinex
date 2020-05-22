d17bf83390003fb84e39f27462a8503964d7172d
Minyus <yusuke.minami86@gmail.com>
2020-05-22 14:19:32 +0000

    Avoid to read hooks from parameters twice

--- src/pipelinex/framework/context/hooks_in_parameters_context.py
+++ src/pipelinex/framework/context/hooks_in_parameters_context.py
@@ -8,6 +8,8 @@ log = getLogger(__name__)
 
 
 class HooksInParametersContext(KedroContext):
+    _hooks_in_params_read = False
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if not hasattr(self, "_hook_manager"):
@@ -52,14 +54,15 @@ class HooksInParametersContext(KedroContext):
     def params(self) -> Dict[str, Any]:
         params = super().params
 
-        assert hasattr(self, "_hook_manager")
+        if not self._hooks_in_params_read:
+            self._hooks_in_params_read = True
 
-        hooks = HatchDict(params).get("HOOKS", [])
-        if not isinstance(hooks, (list, tuple)):
-            hooks = [hooks]
+            hooks = HatchDict(params).get("HOOKS", [])
+            if not isinstance(hooks, (list, tuple)):
+                hooks = [hooks]
 
-        for hook in hooks:
-            if not self._hook_manager.is_registered(hook):
-                self._hook_manager.register(hook)
+            for hook in hooks:
+                if not self._hook_manager.is_registered(hook):
+                    self._hook_manager.register(hook)
 
         return params
