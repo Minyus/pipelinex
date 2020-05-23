4454680ca46cb58d026c848f9902c3ecfcd730db
Minyus <yusuke.minami86@gmail.com>
2020-05-23 10:48:26 +0000

    Add enable_mlflow arg

--- src/pipelinex/extras/hooks/mlflow_outputs_logger.py
+++ src/pipelinex/extras/hooks/mlflow_outputs_logger.py
@@ -13,24 +13,28 @@ if find_spec("mlflow"):
     from mlflow import log_metric, log_param
 
     class MLflowOutputsLoggerHook:
+        def __init__(self, enable_mlflow=True):
+            self.enable_mlflow = enable_mlflow
+
         @hook_impl
         def after_node_run(self, node, catalog, inputs, outputs):
-            for name, value in outputs.items():
-                if isinstance(value, str):
-                    log_param(name, value[:250])
-                    continue
-                if isinstance(value, (float, int)):
-                    log_metric(name, float(value))
-                    continue
-                if isinstance(value, (list, tuple)):
-                    if all([isinstance(e, str) for e in value]):
-                        log_param(name, ", ".join(value))
+            if self.enable_mlflow:
+                for name, value in outputs.items():
+                    if isinstance(value, str):
+                        log_param(name, value[:250])
                         continue
-                    for i, e in enumerate(value):
-                        if isinstance(e, (float, int)):
-                            log_metric(name, float(e), step=i)
-                        else:
-                            break
+                    if isinstance(value, (float, int)):
+                        log_metric(name, float(value))
+                        continue
+                    if isinstance(value, (list, tuple)):
+                        if all([isinstance(e, str) for e in value]):
+                            log_param(name, ", ".join(value))
+                            continue
+                        for i, e in enumerate(value):
+                            if isinstance(e, (float, int)):
+                                log_metric(name, float(e), step=i)
+                            else:
+                                break
 
 
 else:
