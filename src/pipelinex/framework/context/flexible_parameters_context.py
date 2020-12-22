a38a8d08fb8615658c403c1d66a7cea9d77d9a47
Minyus <yusuke.minami86@gmail.com>
2020-12-22 05:56:54 +0000

    Ignore HOOKS in parameters.yml if Kedro 0.17.x

--- src/pipelinex/framework/context/flexible_parameters_context.py
+++ src/pipelinex/framework/context/flexible_parameters_context.py
@@ -2,16 +2,17 @@ from typing import Any, Dict  # NOQA
 from importlib import import_module
 from logging import getLogger
 
+import kedro
 from kedro.pipeline import Pipeline  # NOQA
 
 from pipelinex import HatchDict
-from .hooks_in_parameters_context import HooksInParametersContext
+from .context import KedroContext, get_hook_manager
 
 
 log = getLogger(__name__)
 
 
-class FlexibleParametersContext(HooksInParametersContext):
+class FlexibleParametersContext(KedroContext):
     _params = None
 
     def __init__(self, *args, **kwargs):
@@ -20,10 +21,40 @@ class FlexibleParametersContext(HooksInParametersContext):
         import_modules(params.pop("IMPORT", []))
 
         params = HatchDict(params).get()
-        self._register_kedro_hooks(params.pop("HOOKS", None) or [])
+        hooks = params.pop("HOOKS", None)
+        if hooks:
+            if kedro.__version__.startswith("0.16."):
+                self._hooks_in_params_read = False
+                self._hook_manager = getattr(self, "_hook_manager", get_hook_manager())
+                self._register_kedro_hooks(hooks)
+            else:
+                log.warning(
+                    "HOOKS defined in config file is ignored as the installed kedro version is not 0.16.x"
+                )
         self._kedro_pipelines = params.pop("PIPELINES", None)
         self._kedro_run_config = params.pop("RUN_CONFIG", None) or {}
         self._params = params
+        log.info("RUN_CONFIG: \n{}".format(self._kedro_run_config))
+        log.debug("PIPELINES: \n{}".format(self._kedro_pipelines))
+        log.debug("params: \n{}".format(self._params))
+
+    def _register_kedro_hooks(self, hooks):
+
+        if not self._hooks_in_params_read:
+            self._hooks_in_params_read = True
+
+            if not isinstance(hooks, (list, tuple)):
+                hooks = [hooks]
+
+            for hook in hooks:
+                if not self._hook_manager.is_registered(hook):
+                    self._hook_manager.register(hook)
+                    log.info(
+                        "Registered {} with args: {}".format(
+                            getattr(getattr(hook, "__class__", None), "__name__", None),
+                            getattr(hook, "__dict__", None),
+                        )
+                    )
 
     @property
     def params(self) -> Dict[str, Any]:
