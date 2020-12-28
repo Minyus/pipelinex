315b8b33b9fdc7dd1a4706b46b708c3f07c554f4
Minyus <yusuke.minami86@gmail.com>
2020-12-28 16:06:11 +0000

    Add options to whether to log using MLflowBasicLoggerHook

--- src/pipelinex/extras/hooks/mlflow/mlflow_basic_logger.py
+++ src/pipelinex/extras/hooks/mlflow/mlflow_basic_logger.py
@@ -2,7 +2,7 @@ from importlib.util import find_spec
 from logging import getLogger
 from datetime import datetime, timedelta
 import time
-from typing import Any, Dict  # NOQA
+from typing import Any, Dict, List, Union  # NOQA
 
 from kedro.io import DataCatalog
 from kedro.pipeline import Pipeline
@@ -45,7 +45,11 @@ class MLflowBasicLoggerHook:
         experiment_name: str = None,
         artifact_location: str = None,
         run_name: str = None,
-        offset_hours: float = None,
+        offset_hours: float = 0,
+        enable_logging_time_begin: bool = True,
+        enable_logging_time_end: bool = True,
+        enable_logging_time: bool = True,
+        logging_kedro_run_params: Union[List[str], str] = [],
     ):
         """
         Args:
@@ -59,7 +63,12 @@ class MLflowBasicLoggerHook:
             artifact_location: `artifact_location` arg fed to:
                 https://www.mlflow.org/docs/latest/python_api/mlflow.html#mlflow.create_experiment
             run_name: Shown as 'Run Name' in MLflow UI.
-            offset_hours: The offset hour (e.g. 0 for UTC+00:00) to log in MLflow.
+            offset_hours: The offset hour (e.g. 0 for UTC+00:00) to log in MLflow. 0 in default.
+            enable_logging_time_begin: Enable logging the time the Kedro pipeline began. True in default.
+            enable_logging_time_end: Enable logging the time the Kedro pipeline ended. True in default.
+            enable_logging_time: Enable logging the time duration the Kedro pipeline ran. True in default.
+            logging_kedro_run_params: List of Kedro Run Params to log to MLflow or "__ALL__" to log all.
+                [] (Empty) in default.
 
         """
         self.enable_mlflow = find_spec("mlflow") and enable_mlflow
@@ -67,7 +76,11 @@ class MLflowBasicLoggerHook:
         self.experiment_name = experiment_name
         self.artifact_location = artifact_location
         self.run_name = run_name
-        self.offset_hours = offset_hours or 0
+        self.offset_hours = offset_hours
+        self.enable_logging_time_begin = enable_logging_time_begin
+        self.enable_logging_time_end = enable_logging_time_end
+        self.enable_logging_time = enable_logging_time
+        self.logging_kedro_run_params = logging_kedro_run_params
 
     @hook_impl
     def after_catalog_created(self):
@@ -83,33 +96,52 @@ class MLflowBasicLoggerHook:
     def before_pipeline_run(
         self, run_params: Dict[str, Any], pipeline: Pipeline, catalog: DataCatalog
     ):
-        run_params_renamed = {("___" + k): v for (k, v) in run_params.items()}
-        mlflow_log_params(run_params_renamed, enable_mlflow=self.enable_mlflow)
+        if self.logging_kedro_run_params:
+            run_params_renamed = {
+                ("___" + k): v
+                for (k, v) in run_params.items()
+                if (
+                    isinstance(self.logging_kedro_run_params, str)
+                    and self.logging_kedro_run_params == "__ALL__"
+                )
+                or k in self.logging_kedro_run_params
+            }
+            mlflow_log_params(run_params_renamed, enable_mlflow=self.enable_mlflow)
 
-        timestamp, timestamp_int = get_timestamps(offset_hours=self.offset_hours)
-        time_dict = {"__time_begin": timestamp}
-        time_int_dict = {"__time_begin": timestamp_int}
+        if self.enable_logging_time_begin:
+            timestamp, timestamp_int = get_timestamps(offset_hours=self.offset_hours)
+            time_dict = {"__time_begin": timestamp}
+            time_int_dict = {"__time_begin": timestamp_int}
 
-        mlflow_log_params(time_dict, enable_mlflow=self.enable_mlflow)
-        mlflow_log_metrics(time_int_dict, enable_mlflow=self.enable_mlflow)
+            mlflow_log_params(time_dict, enable_mlflow=self.enable_mlflow)
+            mlflow_log_metrics(time_int_dict, enable_mlflow=self.enable_mlflow)
 
-        self._time_begin = time.time()
+        if self.enable_logging_time:
+            self._time_begin = time.time()
 
     @hook_impl
     def after_pipeline_run(
         self, run_params: Dict[str, Any], pipeline: Pipeline, catalog: DataCatalog
     ):
-        timestamp, timestamp_int = get_timestamps(offset_hours=self.offset_hours)
-        time_dict = {"__time_end": timestamp}
-        time_int_dict = {"__time_end": timestamp_int}
+        if self.enable_logging_time_end:
+            timestamp, timestamp_int = get_timestamps(offset_hours=self.offset_hours)
+            time_dict = {"__time_end": timestamp}
+            time_int_dict = {"__time_end": timestamp_int}
 
-        mlflow_log_params(time_dict, enable_mlflow=self.enable_mlflow)
-        mlflow_log_metrics(time_int_dict, enable_mlflow=self.enable_mlflow)
+            mlflow_log_params(
+                time_dict,
+                enable_mlflow=self.enable_mlflow,
+            )
+            mlflow_log_metrics(
+                time_int_dict,
+                enable_mlflow=self.enable_mlflow,
+            )
 
-        self._time_end = time.time()
-        self._time = self._time_end - self._time_begin
+        if self.enable_logging_time:
+            self._time_end = time.time()
+            self._time = self._time_end - self._time_begin
 
-        time_dict = {"__time": self._time}
-        mlflow_log_metrics(time_dict, enable_mlflow=self.enable_mlflow)
+            time_dict = {"__time": self._time}
+            mlflow_log_metrics(time_dict, enable_mlflow=self.enable_mlflow)
 
         mlflow_end_run(self.enable_mlflow)
