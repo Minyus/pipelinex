e4a6c81a11872cfbe03d895660549220df373bf1
Minyus <Minyus@users.noreply.github.com>
2021-12-25 23:28:04 +0800

    Flatten list elements in dict

--- src/pipelinex/hatch_dict/hatch_dict.py
+++ src/pipelinex/hatch_dict/hatch_dict.py
@@ -233,7 +233,7 @@ def dot_flatten(d):
     try:
         from flatten_dict import flatten
 
-        d = flatten(d, reducer="dot")
+        d = flatten(d, reducer="dot", enumerate_types=(list,))
     except Exception:
         log.warning(
             "{} failed to be flattened. To install dependency, you can run: pip install flatten-dict>=0.3.0".format(
